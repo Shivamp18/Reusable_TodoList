1afe1ea1291d3f07236862970b020b2323ce243e
Shivamp18 <s46hivam17@gmail.com>
2025-11-12 17:56:08 +0530

    Test deploy using GitHub Actions

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-
 
 function ErrorMessage({ message }) {
 
     if (message) {
-        return <div className="bg-red-300 border border-red-500  py-3 rounded w-50 mb-4 ml-25">
+        return <div className="flex  justify-center items-center">
+            <div className=" bg-red-200 border border-red-600  p-3 rounded mb-4">
             <p className="text-red-800 font-bold text-sm text-center">{message}</p>
+        </div>
         </div>
     }
 
